af0d3fbfe27428cf754a0d0fc9aada837e340905
ScisyhpG <121921883+ScisyhpG@users.noreply.github.com>
2026-03-31 12:03:52 +1100

    Add passthrough copy for PHP files
    Added configuration to pass PHP files through to the output.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/data");
+  
+  // NEW: Tell Eleventy to pass any PHP files directly to the _site folder
+  eleventyConfig.addPassthroughCopy("src/**/*.php");
 
   return {
     dir: {
